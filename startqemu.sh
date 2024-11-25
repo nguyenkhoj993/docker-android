2401f06fb1bba901134668e13343e1aa8d8bbbe5
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-25 09:15:09 -0800

    Update startqemu.sh

--- startqemu.sh
+++ startqemu.sh
@@ -4,4 +4,4 @@ export PREFIX=/data/data/com.termux/files/usr
 qemu-system-x86_64 -machine q35 -m 6144 -smp cpus=8 -cpu qemu64 \
   -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
   -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net,netdev=mynet0 \
-  -nographic /data/data/com.termux/files/home/docker-qemu-arm/alpine/alpine.img
+  -nographic /data/data/com.termux/files/home/docker-android/alpine/alpine.img

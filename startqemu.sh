eebccc37f8fc9d50c1ca4c60b74f7a473ac33599
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-25 10:40:55 -0800

    Delete startqemu.sh

--- startqemu.sh
+++
@@ -1,7 +0,0 @@
-# termux folder (remove if you aren't using proot-distro)
-export PREFIX=/data/data/com.termux/files/usr
-
-qemu-system-x86_64 -machine q35 -m 6144 -smp cpus=8 -cpu qemu64 \
-  -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net,netdev=mynet0 \
-  -nographic /data/data/com.termux/files/home/docker-android/alpine/alpine.img
